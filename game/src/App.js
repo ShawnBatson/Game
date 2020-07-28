46f10b97b9c50818f85d0c772852d15c67eb8a21
Shadewalker1984 <Shadewalker1984@yahoo.com>
2020-07-28 18:24:20 -0500

    deleted old stuff,

--- game/src/App.js
+++ game/src/App.js
@@ -17,27 +17,29 @@ const checkNeighbor = [
 
 function App() {
     const [rows, setRows] = useState(40);
-    const [columns, setColumns] = setState(40);
+    const [columns, setColumns] = useState(rows);
     const [speed, setSpeed] = useState(1000);
     const [speedDisplay, setSpeedDisplay] = useState(1);
     const [generation, setGeneration] = useState(1);
     const [size, setSize] = useState(rows * 0.5);
     const [isGameOn, setIsGameOn] = useState(false);
+
     const [grid, setGrid] = useState(() => {
-        const totalrows = [];
+        const totalRows = [];
 
         for (let i = 0; i < rows; i++) {
-            totalrows.push(Array.from(Array(cols), () => 0));
+            totalRows.push(Array.from(Array(columns), () => 0));
         }
         return totalRows;
     });
+
     const spdRef = useRef(speed);
     spdRef.current = speed;
 
     const isGameOnRef = useRef(isGameOn);
     isGameOnRef.current = isGameOn;
 
-    const run = useCallBack(() => {
+    const run = useCallback(() => {
         if (!isGameOnRef.current) {
             return;
         }
@@ -62,7 +64,7 @@ function App() {
                             }
                         });
 
-                        if (neighors < 2 || neighbors > 3) {
+                        if (neighbors < 2 || neighbors > 3) {
                             copy[i][j] = 0; //death
                         } else if (current[i][j] === 0 && neighbors === 3) {
                             copy[i][j] = 1; // life
@@ -78,11 +80,14 @@ function App() {
     return (
         <div className="App">
             <Design
+                rows={rows}
                 size={size}
                 columns={columns}
                 isGameOnRef={isGameOnRef}
                 grid={grid}
                 setGrid={setGrid}
+                speedDisplay={speedDisplay}
+                generation={generation}
             />
         </div>
     );
--- game/src/newGame/design.js
+++ game/src/newGame/design.js
@@ -1,42 +1,55 @@
 import React from "react";
 import produce from "immer";
+
 import "../App.css";
 
 const Design = ({ isGameOnRef, grid, setGrid, size, columns }) => {
     //possibly fix
 
-    const grid = () => {
-        const newGrid = produce(grid, (copy) => {
-            if (isGameOnRef.current) {
-                return;
-            }
-            copy[i][j] = grid[i][j] ? 0 : 1;
-        });
-        setGrid(newGrid);
-    };
+    // const grid = () => {
+    //     const newGrid = produce(grid, (copy) => {
+    //         if (isGameOnRef.current) {
+    //             return;
+    //         }
+    //         copy[i][j] = grid[i][j] ? 0 : 1;
+    //     });
+    //     setGrid(newGrid);
+    // };
     return (
         <div className="gameContainer">
             <div
                 className="container"
                 style={{
                     display: "grid",
-                    gridTemplateColumns: `repeat(${props.columns}, ${props.size}px)`,
+                    gridTemplateColumns: `repeat(${columns}, ${size}px)`,
                 }}
             >
-                {grid.map((columns, i) => {
-                    return (
-                        <div
-                            key={`${i} - ${j}`}
-                            onClick={grid}
-                            style={{
-                                width: size,
-                                height: size,
-                                backgroundColor: (grid[i][j] === 1) & "black",
-                                border: "1px solid black",
-                            }}
-                        />
-                    );
+                {grid.map((rows, i) => {
+                    return rows.map((col, j) => {
+                        return (
+                            <div
+                                key={`${i} - ${j}`}
+                                onClick={() => {
+                                    const newG = produce(grid, (copy) => {
+                                        if (isGameOnRef.current) {
+                                            return;
+                                        }
+                                        copy[i][j] = grid[i][j] === 1 ? 0 : 1;
+                                    });
+                                    setGrid(newG);
+                                }}
+                                style={{
+                                    width: size,
+                                    height: size,
+                                    backgroundColor:
+                                        (grid[i][j] === 1) & "black",
+                                    border: "1px solid black",
+                                }}
+                            />
+                        );
+                    });
                 })}
+                )
             </div>
         </div>
     );

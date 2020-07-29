25a0d1dc997cec099631c4f23b9dca397119b3dc
Shadewalker1984 <Shadewalker1984@yahoo.com>
2020-07-28 20:03:43 -0500

    wrapping up for the night

--- game/src/newGame/control.js
+++ game/src/newGame/control.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { clear } from "./functions";
+import { random } from "./functions";
 import produce from "immer";
 import "../App.css";
 
@@ -43,66 +45,32 @@ const Cont = ({
                 {isGameOnRef ? "Stop" : "Start"}
             </button>
             <button
-                onClick={(
-                    setSpeed,
-                    setGrid,
-                    rows,
-                    columns,
-                    setIsGameOn,
-                    isGameOnRef,
-                    setGeneration,
-                    setSpeedDisplay
-                ) => {
-                    if (isGameOnRef) {
-                        alert("stop");
-                        return;
-                    }
-                    setIsGameOn = false;
-                    setGeneration = 1;
-                    setSpeedDisplay = 1;
-                    setSpeed = 1000;
-
-                    setGrid = (current) => {
-                        return produce(current, (copy) => {
-                            for (let i = 0; i < rows; i++) {
-                                for (let j = 0; j < columns; j++) {
-                                    copy[i][j] = 0;
-                                }
-                            }
-                        });
-                    };
-                }}
+                onClick={() =>
+                    clear(
+                        setSpeed,
+                        setGrid,
+                        rows,
+                        columns,
+                        setIsGameOn,
+                        isGameOnRef,
+                        setGeneration,
+                        setSpeedDisplay
+                    )
+                }
             >
                 Clear
             </button>
             <button
-                onclick={(
-                    setIsGameOn,
-                    isGameOnRef,
-                    setGeneration,
-                    rows,
-                    columns,
-                    setGrid
-                ) => {
-                    if (isGameOnRef.current) {
-                        alert("stop");
-                        return;
-                    }
-                    setIsGameOn("false");
-                    setGeneration(1);
-
-                    setGrid((current) => {
-                        return produce(current, (copy) => {
-                            for (let i = 0; i < rows; i++) {
-                                for (let j = 0; j < columns; j++) {
-                                    Math.random() < 0.5
-                                        ? (copy[i][j] = 0)
-                                        : (copy[i][j] = 1);
-                                }
-                            }
-                        });
-                    });
-                }}
+                onclick={() =>
+                    random(
+                        setIsGameOn,
+                        isGameOnRef,
+                        setGeneration,
+                        rows,
+                        columns,
+                        setGrid
+                    )
+                }
             >
                 Random
             </button>
